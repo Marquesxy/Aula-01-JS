0fdf3f4119aff1df25c6a3ac72e36b76b2bc5aef
Marquesxy <mairon.msantos@senacsp.edu.br>
2024-08-21 20:07:56 -0300

    Lição feita 20/08

--- script.js
+++ script.js
@@ -12,26 +12,20 @@ nota3 = parseFloat(nota3);
 var nota4 = window.prompt ("Digite a nota 4");
 nota4 = parseFloat(nota4);
 
-var mediaA = (nota1 + nota2 + nota3 + nota4) /4;
+var media = (nota1 + nota2 + nota3 + nota4) /4;
 
 var nomeA = window.prompt ("Informe o Nome do Aluno 2:");
 
 var nota01 = window.prompt ("Digite a nota do aluno 2 N°1");
 nota01 = parseFloat(nota01);
-
 var nota02 = window.prompt ("Digite  nota do aluno 2 N°2");
 nota02 = parseFloat(nota02);
-
 var nota03 = window.prompt ("Digite  nota do aluno 2 N°3");
 nota03 = parseFloat(nota03);
-
 var nota04 = window.prompt ("Digite  nota do aluno N°4");
 nota04 = parseFloat(nota04);
 
 var mediaA = (nota01 + nota02 + nota03 + nota04) /4;
 
-var mensagem = nome + ", Você obteve a média:   " + media;
-window.alert(mensagem);
-
-var mensagem = nomeA + ", Você obteve a média:   " + mediaA;
+var mensagem = nome + ", aluno 1 obteve a média:   " + media + "e" + nomeA + ", aluno 2 obteve a nota" + mediaA;
 window.alert(mensagem);
