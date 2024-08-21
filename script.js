612137e5ecb9e402986bfd5df55a208b7d35934e
Marquesxy <mairon.msantos@senacsp.edu.br>
2024-08-20 22:14:56 -0300

    feat: Exercicios

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-//Exibir no Terminal
-console.log("Olá Mundo");
-// Exibir Navegador
-document.write("Olá Navegador!");
-//Um Alerte 
-//alert("Alerta de Confirmação!");
-/*Comentario ee Bloco*/
-/*parseFloat: retorna variaveis de texto em decimais*/
+/*var nome = window.prompt ("Informe o Nome do Aluno 1:");
 
-var nome = window.prompt ("Informe o Nome:");
-//Guardar o nome do Aluno
 var nota1 = window.prompt ("Digite a nota 1");
-//guarda a primeira nota
 nota1 = parseFloat(nota1);
-//converte de texto para decimal
+
 var nota2 = window.prompt ("Digite a nota 2");
-//guarda a segunda nota
 nota2 = parseFloat(nota2);
-// converte de texto para decimal
-var media = (nota1 + nota2) /2;
-//a soma +  a media das duas
+
+var nota3 = window.prompt ("Digite a nota 3");
+nota3 = parseFloat(nota3);
+
+var nota4 = window.prompt ("Digite a nota 4");
+nota4 = parseFloat(nota4);
+
+var media = (nota1 + nota2 + nota3 + nota4) /4;*/
+
+var nome = window.prompt ("Informe o Nome do Aluno 2:");
+
+var nota1. = window.prompt ("Digite a nota do aluno 2 N°1");
+nota1. = parseFloat(nota1.);
+
+var nota2. = window.prompt ("Digite  nota do aluno 2 N°2");
+nota2. = parseFloat(nota2.);
+
+var nota3. = window.prompt ("Digite  nota do aluno 2 N°3");
+nota3. = parseFloat(nota3.);
+
+var nota4 = window.prompt ("Digite  nota do aluno N°4");
+nota4. = parseFloat(nota4.);
+
+var media = (nota1. + nota2. + nota3. + nota4.) /4;
+
 var mensagem = nome + ", Você obteve a média:   " + media;
 window.alert(mensagem);
-//gera a mensagem de exibição do nome e da media do aluno
